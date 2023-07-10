6e4dc22b4d56f6bd74fa8900f64593cf7f83d03d
Ben Kalsky <benkalsky@gmail.com>
2023-07-11 00:08:11 +0300

    Update monitorStatusHeader.js

--- src/components/monitorStatusHeader.js
+++ src/components/monitorStatusHeader.js
@@ -23,10 +23,10 @@ export default function MonitorStatusHeader({ kvMonitorsLastUpdate }) {
         <div>{text}</div>
         {kvMonitorsLastUpdate.time && typeof window !== 'undefined' && (
           <div className="text-xs font-light">
-            checked{' '}
+            Checked{' '}
             {Math.round((Date.now() - kvMonitorsLastUpdate.time) / 1000)} sec
-            ago (from{' '}
-            {locations[kvMonitorsLastUpdate.loc] || kvMonitorsLastUpdate.loc})
+            ago from{' '}
+            {locations[kvMonitorsLastUpdate.loc] || kvMonitorsLastUpdate.loc}
           </div>
         )}
       </div>
