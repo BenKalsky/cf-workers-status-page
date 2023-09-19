5b2b577d5ed779c59f27df5198f4598c888a831c
Ben Kalsky <benkalsky@gmail.com>
2023-09-19 23:59:52 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
       <Head>
         <title>{config.settings.title}</title>
         <link rel="stylesheet" href="./style.css" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script>
           {`
           function setTheme(theme) {
